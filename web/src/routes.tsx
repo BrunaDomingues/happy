82855f7db0e048944148228e4aa1e52a9265b2e3
BrunaDomingues <brunadomingues.dev@gmail.com>
2020-10-14 18:38:34 -0300

    Criando as rotas

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,1 +1,13 @@
-import {} from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import Landing from './pages/landing';
+import OrphanageMaps from './pages/OrphanagesMap';
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Route path="/" component={Landing}/>
+            <Route path="/app" component={OrphanageMaps}/>
+        </BrowserRouter>
+    );
+}
+export default Routes;

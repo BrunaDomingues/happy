bb93cce98185aaa8b691931d16fd60e3f89e267a
BrunaDomingues <brunadomingues.dev@gmail.com>
2020-10-14 12:14:31 -0300

    Criando e estilizando a Landing Page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import './styles/global.css';
+import './styles/pages/landing.css';
+import logoImg from './images/logo.svg';
+import {FiArrowRight} from 'react-icons/fi';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt=""/>
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+        <div className="location">
+          <strong>Bagé</strong>
+          <span>Rio Grande do Sul</span>
+        </div>
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0.0.0,0.6)"/>
+        </a>
+      </div>
     </div>
   );
 }
-
 export default App;

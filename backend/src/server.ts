cfcba81923fb0b3d7c1a3c4e92dcdbc20f7b78af
BrunaDomingues <brunadomingues.dev@gmail.com>
2020-10-15 11:13:28 -0300

    Começando bando de dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,19 @@
 import express from 'express';
 const app = express();
-app.listen(3333);
-app.get("/users", (request, response) => {
+app.use(express.json());
+// Rota = Conjunto
+// Recurso = usuário
+// Métodos http = GET, POST, PUT, DELETE
+// Parametros
+// GET - Buscar uma info
+// POST - Criando uma info
+// PUT - Editar uma info
+// DELETE - Delete uma info
+// /users/:id : => parametro
+app.post("/users/:id", (request, response) => {
+    console.log(request.query);
+    console.log(request.params);
+    console.log(request.body);
     return response.json({message: "Hello World"});
 });
+app.listen(3333);
